310cdb28f757b09d1f859dd09bfadaebf1625a60
Divya Reddy Sadhula <146372017+DivyaReddySadhula@users.noreply.github.com>
2025-05-15 10:51:16 -0400

    Time: 993 ms (14.32%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+select 
+to_char(activity_date,'YYYY-MM-DD') as day
+,count(distinct user_id)  as active_users
+from Activity 
+where activity_date  between (to_date('2019-07-27','YYYY-MM-DD')-29) and to_date('2019-07-27','YYYY-MM-DD')
+group by activity_date  
+order by activity_date
