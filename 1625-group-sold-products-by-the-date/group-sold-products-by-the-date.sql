277f2136fada35d9613df9f771ea044b4e554c01
Divya Reddy Sadhula <146372017+DivyaReddySadhula@users.noreply.github.com>
2025-05-19 11:22:47 -0400

    Time: 506 ms (64.22%) | Memory: 0B (100.00%) - LeetSync

--- 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
+++ 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -1,7 +1,7 @@
 /* Write your PL/SQL query statement below */
 select
 to_char(sell_date) as sell_date
-,count(distinct product) as num_sold
+,count( product) as num_sold
 ,LISTAGG(product, ',') WITHIN GROUP (ORDER BY product) as products
 from 
 (select distinct sell_date,product from Activities)
