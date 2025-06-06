b5451baa46a39869e179e2776ab1acfaf4e73134
Divya Reddy Sadhula <146372017+DivyaReddySadhula@users.noreply.github.com>
2025-06-06 17:31:31 -0400

    Time: 842 ms (25.58%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -5,11 +5,10 @@ with lastPerson as
     turn
     ,person_name
     ,sum(weight) over(order by turn asc) as cumSum
-    ,row_number() over(order by turn ) as rnk
     from Queue
 )select person_name
 from 
-(select person_name,turn
+(select person_name
  from lastPerson
  where  cumSum<=1000
  order by turn desc
