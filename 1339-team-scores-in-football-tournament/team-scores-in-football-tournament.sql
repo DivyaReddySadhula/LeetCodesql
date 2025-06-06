61645d4f89084be6a5b441ec63f21c5813e5a864
Divya Reddy Sadhula <146372017+DivyaReddySadhula@users.noreply.github.com>
2025-06-06 18:59:45 -0400

    Time: 750 ms (82.35%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1339-team-scores-in-football-tournament/team-scores-in-football-tournament.sql
@@ -0,0 +1,38 @@
+/* Write your PL/SQL query statement below */
+WITH score AS (
+    -- Points when the team is host
+    SELECT 
+        m.host_team AS team_id,
+        CASE 
+            WHEN m.host_goals > m.guest_goals THEN 3
+            WHEN m.host_goals = m.guest_goals THEN 1
+            ELSE 0
+        END AS points
+    FROM matches m
+
+    UNION ALL
+
+    -- Points when the team is guest
+    SELECT 
+        m.guest_team AS team_id,
+        CASE 
+            WHEN m.guest_goals > m.host_goals THEN 3
+            WHEN m.guest_goals = m.host_goals THEN 1
+            ELSE 0
+        END AS points
+    FROM matches m
+),
+points_by_team AS (
+    SELECT 
+        team_id,
+        SUM(points) AS num_points
+    FROM score
+    GROUP BY team_id
+)
+SELECT 
+    t.team_id,
+    t.team_name,
+    NVL(p.num_points, 0) AS num_points
+FROM teams t
+LEFT JOIN points_by_team p ON t.team_id = p.team_id
+ORDER BY num_points DESC, t.team_id ASC;
