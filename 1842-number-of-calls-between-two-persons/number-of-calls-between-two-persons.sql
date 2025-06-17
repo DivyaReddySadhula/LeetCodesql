05415a7c56e2d923858db981fa828c8a706a10e1
Divya Reddy Sadhula <146372017+DivyaReddySadhula@users.noreply.github.com>
2025-06-17 10:45:43 -0400

    Time: 638 ms (14.29%) | Memory: 0B (100.00%) - LeetSync

--- 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
+++ 1842-number-of-calls-between-two-persons/number-of-calls-between-two-persons.sql
@@ -1,9 +1,13 @@
 /* Write your PL/SQL query statement below */
-select
-least(from_id,to_id) as person1
-,greatest(from_id,to_id) as person2
-,count(*) as call_count
-,sum(duration) as total_duration
-from Calls
-group by least(from_id,to_id),greatest(from_id,to_id)
-order by person1,person2
+with noOfCalls as
+(
+    select
+    case when from_id<to_id then from_id else to_id end as person1,
+    case when from_id<to_id then to_id else from_id end as person2,
+    duration
+    from calls
+)select
+person1,person2,count(*) as call_count,sum(duration) as total_duration
+from noOfCalls
+group by person1,person2
+order by person1
