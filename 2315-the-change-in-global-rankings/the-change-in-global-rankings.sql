c1ef6842b3ca93d4f01d9e57ac6f4fdccb3f5677
Divya Reddy Sadhula <146372017+DivyaReddySadhula@users.noreply.github.com>
2025-06-23 10:58:25 -0400

    Time: 755 ms (60.00%) | Memory: 0B (100.00%) - LeetSync

--- 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
+++ 2315-the-change-in-global-rankings/the-change-in-global-rankings.sql
@@ -1,13 +1,9 @@
 /* Write your PL/SQL query statement below */
-with res as 
-(select
+select
 tp.team_id
 ,tp.name
-,tp.points
-,(tp.points+pc.points_change) as updated_points
-,rank() over(order by tp.points desc,tp.name asc) 
--rank() over(order by (tp.points+pc.points_change) desc,tp.name asc)as rank_diff
+,row_number() over(order by tp.points desc,tp.name asc) 
+-row_number() over(order by (tp.points+pc.points_change) desc,tp.name asc)as rank_diff
 from teampoints tp
 join pointschange pc
 on tp.team_id = pc.team_id
-)select team_id,name,rank_diff from res order by updated_points desc,name asc
