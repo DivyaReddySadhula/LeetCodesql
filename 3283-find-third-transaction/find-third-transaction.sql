1915048247068318ac46dd88c2bdc409ff657e4d
Divya Reddy Sadhula <146372017+DivyaReddySadhula@users.noreply.github.com>
2025-06-30 11:28:24 -0400

    Time: 670 ms (0.00%) | Memory: 0B (100.00%) - LeetSync

--- 3283-find-third-transaction/find-third-transaction.sql
+++ 3283-find-third-transaction/find-third-transaction.sql
@@ -1,30 +1,20 @@
 /* Write your PL/SQL query statement below */
-with cte as 
-(select
-user_id
-,spend
-,transaction_date
-,rank() over(partition by user_id order by transaction_date asc) as rnk
-from transactions
-),third_txn AS (
-    SELECT * FROM cte WHERE rnk = 3
-),
-first_second_txn AS (
-    SELECT * FROM cte WHERE rnk IN (1, 2)
-),
-check_conditions AS (
+WITH ranked_txns AS (
     SELECT
-        t.user_id,
-        t.spend AS third_spend,
-        MAX(f.spend) AS max_prev_spend
-    FROM third_txn t
-    JOIN first_second_txn f ON t.user_id = f.user_id
-    GROUP BY t.user_id, t.spend
+        user_id,
+        spend,
+        transaction_date,
+        RANK() OVER (PARTITION BY user_id ORDER BY transaction_date) AS rnk,
+        LAG(spend, 1) OVER (PARTITION BY user_id ORDER BY transaction_date) AS prev_spend_1,
+        LAG(spend, 2) OVER (PARTITION BY user_id ORDER BY transaction_date) AS prev_spend_2
+    FROM transactions
 )
-SELECT 
-    t.user_id
-    ,t.spend as third_transaction_spend
-    ,t.transaction_date as third_transaction_date  
-FROM third_txn t
-JOIN check_conditions c 
-  ON t.user_id = c.user_id AND t.spend > c.max_prev_spend;
+SELECT
+    user_id,
+    spend as third_transaction_spend,
+    transaction_date as third_transaction_date
+FROM ranked_txns
+WHERE rnk = 3
+  AND spend > prev_spend_1
+  AND spend > prev_spend_2;
+
